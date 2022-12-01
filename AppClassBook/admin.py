792d61bf0a9a6de6cb85ec6cef36825f62932f07
Caroline4941 <carolinacera98@gmail.com>
2022-12-01 19:22:19 -0300

    commit 2

--- AppClassBook/admin.py
+++ AppClassBook/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import *
+
+admin.site.register(Curso)
+admin.site.register(Estudiante)
+admin.site.register(Profesor)
+admin.site.register(Examenes)
+admin.site.register(Tareas)
 
 # Register your models here.
--- AppClassBook/models.py
+++ AppClassBook/models.py
@@ -6,7 +6,7 @@ class Curso(models.Model):
     camada=models.IntegerField()
 
     def __str__(self):
-        return self.nombre+" "+self.camada
+        return self.nombre+" "+str(self.camada)
 
 class Estudiante(models.Model):
     nombre=models.CharField(max_length=50)
@@ -29,9 +29,13 @@ class Tareas(models.Model):
     titulo=models.CharField(max_length=50)
     fechaDeEntrega=models.DateField()
     entregado=models.BooleanField()
+    def __str__(self):
+        return self.titulo+" "+str(self.fechaDeEntrega)
 
 class Examenes(models.Model):
     titulo=models.CharField(max_length=50)
     nota=models.IntegerField()
 
+def __str__(self):
+        return self.titulo+" "+str(self.nota)
 
