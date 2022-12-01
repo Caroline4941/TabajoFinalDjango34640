89fa5e09da1763e98bf069cc45fa7dbe7d51fcb0
Caroline4941 <carolinacera98@gmail.com>
2022-12-01 17:05:31 -0300

    commit1

---
+++ AppClassBook/models.py
@@ -0,0 +1,37 @@
+from django.db import models
+
+# Create your models here.
+class Curso(models.Model):
+    nombre=models.CharField(max_length=50)
+    camada=models.IntegerField()
+
+    def __str__(self):
+        return self.nombre+" "+self.camada
+
+class Estudiante(models.Model):
+    nombre=models.CharField(max_length=50)
+    apellido=models.CharField(max_length=50)
+    libreta=models.IntegerField()
+    email=models.EmailField()
+
+    def __str__(self):
+        return self.nombre+" "+self.apellido
+    
+class Profesor(models.Model):
+    nombre=models.CharField(max_length=50)
+    apellido=models.CharField(max_length=50)
+    email=models.EmailField()
+
+    def __str__(self):
+        return self.nombre+" "+self.apellido
+
+class Tareas(models.Model):
+    titulo=models.CharField(max_length=50)
+    fechaDeEntrega=models.DateField()
+    entregado=models.BooleanField()
+
+class Examenes(models.Model):
+    titulo=models.CharField(max_length=50)
+    nota=models.IntegerField()
+
+
---
+++ AppClassBook/views.py
@@ -0,0 +1,35 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Curso
+from django.template import Template, Context, loader
+# Create your views here.
+
+def curso(request):
+
+    matematicas=Curso(nombre="MatematicasII",camada=123456)
+    matematicas.save()
+
+    return render(request, "AppClassBook/curso.html")
+
+def inicio(request):
+     return render(request,"AppClassBook/inicio.html")
+
+def cursos(request):
+     return render(request,"AppClassBook/cursos.html")
+
+def estudiantes(request):
+     return render(request,"AppClassBook/estudiantes.html")
+
+def profesores(request):
+     return render(request,"AppClassBook/profesores.html")
+
+def tareas(request):
+     return render(request,"AppClassBook/tareas.html")
+
+def examenes(request):
+     return render(request,"AppClassBook/examenes.html")
+
+
+
+
+
